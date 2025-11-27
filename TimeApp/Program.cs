f283755f61a40371ac180fa37bfda58cc215124f
Carlos <carloscortezmarrero@gmail.com>
2025-11-27 13:39:47 +0100

    Añadir UsageSession y cálculo de duración de sesiones

--- TimeApp/Program.cs
+++ TimeApp/Program.cs
@@ -13,13 +13,23 @@ namespace TimeApp
 
             try
             {
-                var instagramApp = new TrackedApp("instagram", "Instagram", 0);
+                var instagramApp = new TrackedApp("instagram", "Instagram", 12);
 
                 Console.WriteLine($"Identificador: {instagramApp.Id}, Nombre: {instagramApp.Name}," +
                     $" Límite de tiempo: {instagramApp.DailyLimitMinutes}");
-               
+
+                // Inicio: 27/11/2025 a las 13:06
+                var startTime = new DateTime(2025, 11, 27, 13, 06, 00);
+
+                // Fin: 27/11/2025 a las 15:15
+                var endTime = new DateTime(2025, 11, 27, 15, 15, 00);
+                var usageInstagram= new UsageSession(instagramApp.Id,startTime,endTime);
+
+                
+                Console.WriteLine($"Duración de la sesión: {usageInstagram.GetDurationMinutes()} minutos");
+
             }
-            catch(ArgumentOutOfRangeException e)
+            catch (ArgumentOutOfRangeException e)
             {
                 Console.WriteLine(e.Message);
             }
---
+++ TimeApp/UsageSession.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TimeApp
+{
+    public class UsageSession
+    {
+        public string AppId { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+        public UsageSession(string appId, DateTime startTime, DateTime endTime)
+        {
+            this.AppId = appId;
+            this.StartTime = startTime;
+            if(endTime < startTime)
+            {
+                throw new ArgumentOutOfRangeException(nameof(endTime),
+                    "La hora de fin no puede ser anterior a la hora de inicio.");
+            }
+            this.EndTime = endTime;
+        }
+
+        public int GetDurationMinutes()
+        {
+            
+          TimeSpan interval=EndTime-StartTime;
+            return (int)interval.TotalMinutes;
+        }
+    }
+}
