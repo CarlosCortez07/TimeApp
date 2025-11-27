5e094907dc1765992ef20f34219082887f323ab2
Carlos <carloscortezmarrero@gmail.com>
2025-11-27 12:44:20 +0100

    Validar límite  diario en TrackedApp y manejar excepción en Main

--- TimeApp/Program.cs
+++ TimeApp/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeApp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            try
+            {
+                var instagramApp = new TrackedApp("instagram", "Instagram", 0);
+
+                Console.WriteLine($"Identificador: {instagramApp.Id}, Nombre: {instagramApp.Name}," +
+                    $" Límite de tiempo: {instagramApp.DailyLimitMinutes}");
+               
+            }
+            catch(ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+         
+
+
+        }
+
+
+
+
+
+    }
+  
+
+}
---
+++ TimeApp/TrackedApp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeApp
+{
+    public class TrackedApp
+    {
+        public string Id{get; set;}
+        public string Name { get; set;}
+        public int DailyLimitMinutes {  get; set;}
+
+        public TrackedApp(string id, string name, int dailyLimitMinutes)
+        {
+            Id = id;
+            Name = name;
+
+            if (dailyLimitMinutes <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(dailyLimitMinutes),
+                    "El límite diario debe ser mayor que cero");
+            }
+            
+            DailyLimitMinutes=dailyLimitMinutes;
+            
+
+
+        }
+    }
+}
