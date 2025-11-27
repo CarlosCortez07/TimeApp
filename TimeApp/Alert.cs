b72f20c7adf347e0ec7be873355583d4baef7538
Carlos <carloscortezmarrero@gmail.com>
2025-11-27 18:21:21 +0100

    Creación de clase Alert e implementación del metodo         public static Alert? GetDailyAlert(TrackedApp app, DateTime day, List<UsageSession>sessions)

---
+++ TimeApp/Alert.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeApp
+{
+    public class Alert
+    {
+        public string AppName { get; set; }
+        public DateTime Day { get; set; }
+        public int UsedMinutes { get; set; }
+        public int LimitMinutes { get; set; }
+        public string Message { get; set; }
+
+        public static Alert? GetDailyAlert(TrackedApp app, DateTime day, List<UsageSession>sessions)
+        {
+            int totalMinutes = 0;
+            foreach(var session in sessions)
+            {
+                if(session.AppId==app.Id && session.StartTime.Date==day.Date)
+                {
+                    totalMinutes += session.GetDurationMinutes();
+                }
+            }
+            if (totalMinutes > app.DailyLimitMinutes)
+            {
+                return new Alert
+                {
+                    AppName = app.Name,
+                    Day = day.Date,
+                    UsedMinutes = totalMinutes,
+                    LimitMinutes = app.DailyLimitMinutes,
+                    Message = $"Has usado {totalMinutes} minutos (límite {app.DailyLimitMinutes})."
+                };
+            }
+
+            // No hay alerta
+            return null;
+
+        }
+
+    }
+}
--- TimeApp/Program.cs
+++ TimeApp/Program.cs
@@ -23,29 +23,31 @@ namespace TimeApp
                 var day = new DateTime(2025, 11, 27);
 
                 var start1 = new DateTime(2025,11,27,9,00,00);
-                var end1 = new DateTime(2025, 11, 27, 9, 30, 00);
+                var end1 = new DateTime(2025, 11, 27, 9, 03, 00);
 
                 var session1 = new UsageSession(instagramApp.Id,start1,end1);
                 sessions.Add(session1);
                 Console.WriteLine($"Duración de la sesión: {session1.GetDurationMinutes()} minutos");
 
                 var start2 = new DateTime(2025, 11, 27, 13, 06, 00);
-                var end2 = new DateTime(2025, 11, 27, 15, 15, 00);
+                var end2 = new DateTime(2025, 11, 27, 13, 07, 00);
 
                 var session2 = new UsageSession(instagramApp.Id, start2, end2);
                 sessions.Add(session2);
                 Console.WriteLine($"Duración de la sesión: {session2.GetDurationMinutes()} minutos");
 
-                var minutosTotal = CalculateDailyUsageMinutes(instagramApp.Id, day, sessions);
 
-                Console.WriteLine($"Has usado {instagramApp.Name} {minutosTotal} minutos el {day:dd/MM/yyyy}");
-                if (minutosTotal > instagramApp.DailyLimitMinutes)
+                var alert = Alert.GetDailyAlert(instagramApp, day, sessions);
+
+                if (alert != null)
                 {
-                    Console.WriteLine("Has superado el límite diario de esta app.");
+                    Console.WriteLine($"ALERTA para {alert.AppName} el {alert.Day:dd/MM/yyyy}");
+                    Console.WriteLine(alert.Message);
+                    Console.WriteLine($"Usado: {alert.UsedMinutes} minutos (límite: {alert.LimitMinutes})");
                 }
                 else
                 {
-                    Console.WriteLine("Estás dentro del límite diario.");
+                    Console.WriteLine("No hay alerta, estás dentro del límite diario.");
                 }
 
             }
@@ -62,7 +64,7 @@ namespace TimeApp
 
         }
 
-        public static int CalculateDailyUsageMinutes(string appid, DateTime day, List <UsageSession> sessions)
+        private static int CalculateDailyUsageMinutes(string appid, DateTime day, List <UsageSession> sessions)
         {
             int minutosTotal = 0;
 
