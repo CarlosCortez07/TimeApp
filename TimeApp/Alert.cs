f56c7ea0a3bd9f1961af9d0ddc9d96edf9b30b81
Carlos <carloscortezmarrero@gmail.com>
2025-12-01 12:18:19 +0100

    Cambios

--- TimeApp/Alert.cs
+++ TimeApp/Alert.cs
@@ -8,37 +8,49 @@ namespace TimeApp
 {
     public class Alert
     {
-        public string AppName { get; set; }
+        public required string AppName { get; set; }
         public DateTime Day { get; set; }
         public int UsedMinutes { get; set; }
         public int LimitMinutes { get; set; }
-        public string Message { get; set; }
+        public required string Message { get; set; }
 
         public static Alert? GetDailyAlert(TrackedApp app, DateTime day, List<UsageSession>sessions)
         {
-            int totalMinutes = 0;
-            foreach(var session in sessions)
-            {
-                if(session.AppId==app.Id && session.StartTime.Date==day.Date)
-                {
-                    totalMinutes += session.GetDurationMinutes();
-                }
-            }
-            if (totalMinutes > app.DailyLimitMinutes)
-            {
-                return new Alert
-                {
-                    AppName = app.Name,
-                    Day = day.Date,
-                    UsedMinutes = totalMinutes,
-                    LimitMinutes = app.DailyLimitMinutes,
-                    Message = $"Has usado {totalMinutes} minutos (límite {app.DailyLimitMinutes})."
-                };
-            }
+            int totalMinutes = GetAppMinutes(app, day, sessions);
+
+         
+                    if (totalMinutes > app.DailyLimitMinutes)
+                    {
+                        return new Alert
+                        {
+                            AppName = app.Name,
+                            Day = day.Date,
+                            UsedMinutes = totalMinutes,
+                            LimitMinutes = app.DailyLimitMinutes,
+                            Message = $"Has usado {totalMinutes} minutos (límite {app.DailyLimitMinutes})."
+                        };
+                    }
+               
+            
+      
 
             // No hay alerta
             return null;
 
+        }
+
+        private static int GetAppMinutes(TrackedApp app, DateTime day, List<UsageSession> sessions)
+        {
+            int totalMinutes = 0;
+            foreach(var session in sessions)
+            {
+                if(session.AppId == app.Id &&  session.StartTime.Date==day.Date)
+                {
+                  totalMinutes += session.GetDurationMinutes();
+                }
+             
+            }
+            return totalMinutes;
         }
 
     }
--- TimeApp/Program.cs
+++ TimeApp/Program.cs
@@ -13,10 +13,16 @@ namespace TimeApp
 
             try
             {
-                var instagramApp = new TrackedApp("instagram", "Instagram", 12);
+                var instagramApp = new TrackedApp("instagram", "Instagram", 1);
 
                 Console.WriteLine($"Identificador: {instagramApp.Id}, Nombre: {instagramApp.Name}," +
               $" Límite de tiempo: {instagramApp.DailyLimitMinutes}");
+
+                var youtubeApp = new TrackedApp("youtube", "Youtube", 0);
+
+                Console.WriteLine($"Identificador: {youtubeApp.Id}, Nombre: {youtubeApp.Name}," +
+              $" Límite de tiempo: {youtubeApp.DailyLimitMinutes}");
+
 
 
                 var sessions = new List<UsageSession>();
@@ -25,31 +31,48 @@ namespace TimeApp
                 var start1 = new DateTime(2025,11,27,9,00,00);
                 var end1 = new DateTime(2025, 11, 27, 9, 03, 00);
 
-                var session1 = new UsageSession(instagramApp.Id,start1,end1);
-                sessions.Add(session1);
-                Console.WriteLine($"Duración de la sesión: {session1.GetDurationMinutes()} minutos");
+                var sessionInsta1 = new UsageSession(instagramApp.Id,start1,end1);
+                sessions.Add(sessionInsta1);
+             
 
                 var start2 = new DateTime(2025, 11, 27, 13, 06, 00);
                 var end2 = new DateTime(2025, 11, 27, 13, 07, 00);
 
-                var session2 = new UsageSession(instagramApp.Id, start2, end2);
-                sessions.Add(session2);
-                Console.WriteLine($"Duración de la sesión: {session2.GetDurationMinutes()} minutos");
+                var sessionInsta2 = new UsageSession(instagramApp.Id, start2, end2);
+                sessions.Add(sessionInsta2);
+          
+
+                var startYoutube = new DateTime(2025, 11, 27, 9, 00, 00);
+                var endYoutube = new DateTime(2025, 11, 27, 9, 30, 00);
+                var sessionYoutube1= new UsageSession(youtubeApp.Id, startYoutube, endYoutube);
+                sessions.Add(sessionYoutube1);
+
+                var startYoutube2 = new DateTime(2025, 11, 27, 9, 00, 00);
+                var endYoutube2 = new DateTime(2025, 11, 27, 10, 00, 00);
+                var sessionYoutube2 = new UsageSession(youtubeApp.Id, startYoutube2, endYoutube2);
+                sessions.Add(sessionYoutube2);
 
 
-                var alert = Alert.GetDailyAlert(instagramApp, day, sessions);
+                var apps = new List<TrackedApp> {instagramApp,youtubeApp};
+         
 
-                if (alert != null)
+                foreach (var app in apps) {
+
+                    var Alertas = Alert.GetDailyAlert(app, day, sessions);
+
+                    if (Alertas != null)
                 {
-                    Console.WriteLine($"ALERTA para {alert.AppName} el {alert.Day:dd/MM/yyyy}");
-                    Console.WriteLine(alert.Message);
-                    Console.WriteLine($"Usado: {alert.UsedMinutes} minutos (límite: {alert.LimitMinutes})");
+                       
+                        Console.WriteLine($"Alertas para {Alertas.AppName} el {Alertas.Day:dd/MM/yyyy}");
+                    Console.WriteLine(Alertas.Message);
+                    Console.WriteLine($"Usado: {Alertas.UsedMinutes} minutos (límite: {Alertas.LimitMinutes})");
                 }
                 else
                 {
-                    Console.WriteLine("No hay alerta, estás dentro del límite diario.");
+                    Console.WriteLine($"{app.Name} : No hay alerta, estás dentro del límite diario.");
                 }
 
+                }
             }
             catch (ArgumentOutOfRangeException e)
             {
